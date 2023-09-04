ebde2eae27fc0ce5c79888d342a1f26f168d02be
SUBIN S <subinvidhu3232@gmail.com>
2023-09-04 14:16:21 +0530

    readline and readlines

--- CODES/read_file.py
+++ CODES/read_file.py
@@ -1,9 +1,29 @@
+# Normal (read)
 f = open('C:/Users/Subin-PC/Documents/Flask Local Host to aira_aramis_ai.txt','r')
 
 data = f.read()
 
 f.close()
 print(data)
+
+# readline
+f = open('C:/Users/Subin-PC/Documents/Flask Local Host to aira_aramis_ai.txt','r')
+
+lines = f.readlines()
+
+for line in lines:
+    print(line)
+f.close()
+
+# readline
+f = open('C:/Users/Subin-PC/Documents/Flask Local Host to aira_aramis_ai.txt','r')
+line = f.readline()
+
+while line:
+    print(line)
+    line = f.readline()
+
+f.close()
 
 # import os.path
 
