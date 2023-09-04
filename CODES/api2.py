5bc3722d216e994d51f8cfdf86be068fe5d40056
SUBIN S <subinvidhu3232@gmail.com>
2023-09-04 16:30:20 +0530

    api endpoint

--- CODES/api2.py
+++ CODES/api2.py
@@ -13,3 +13,26 @@ data = json.loads(response.text)
 for item in data["products"]:
     print(item)
 
+for item in data["products"]:
+    print(item['title'])
+
+
+#
+import requests
+ 
+#Define the API endpoint
+url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY'
+ 
+#Make the request to the API
+response = requests.get(url)
+ 
+#Parse the response
+data = response.json()
+ 
+#Access the data
+temperature = data['main']['temp']
+humidity = data['main']['humidity']
+ 
+#Print the data
+print('Temperature:', temperature)
+print('Humidity:', humidity)
