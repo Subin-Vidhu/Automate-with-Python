39cf63c53653dc8f058f82aa45574cb7c18ab5a2
SUBIN S <subinvidhu3232@gmail.com>
2023-09-04 16:07:39 +0530

    sysex

---
+++ CODES/sysex.py
@@ -0,0 +1,8 @@
+import os
+
+# List files and directories on Linux and Mac
+# if you use Windows, change it to "dir"
+#os.system("ls")
+print(os.system("dir")) # output = 0 means that the command is successful
+#print(os.system("date")) For Linux
+#os.system('date;ls')
