c155e080c7b301002b2b4bd6fa3ddb398204432a
SUBIN S <subinvidhu3232@gmail.com>
2023-09-04 14:09:27 +0530

    reading files

---
+++ CODES/read_file.py
@@ -0,0 +1,18 @@
+f = open('C:/Users/Subin-PC/Documents/Flask Local Host to aira_aramis_ai.txt','r')
+
+data = f.read()
+
+f.close()
+print(data)
+
+# import os.path
+
+# filename = "myfile.txt"
+
+# if os.path.isfile(filename):
+#     with open(filename, "r") as f:
+#         contents = f.read()
+#         print(contents)
+#     else:
+#         print("File does not exist.")
+
